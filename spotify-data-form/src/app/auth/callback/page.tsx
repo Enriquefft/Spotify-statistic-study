476934f0dcc5a9b5eca51d4c7512b2aa338be90e
Enrique <enrique.flores@utec.edu.pe>
2024-01-21 07:58:38 -0500

    Disabled ssr on /auth/callback

--- spotify-data-form/src/app/auth/callback/page.tsx
+++ spotify-data-form/src/app/auth/callback/page.tsx
@@ -1,63 +1,18 @@
 "use client";
 
-import { z } from "zod";
+import dynamic from "next/dynamic";
 
-const responseParamsSchema = z.preprocess(
-  (val) => {
-    if (typeof val !== "string") {
-      throw new Error("Expected a string");
-    }
-
-    return val
-      .substring(1)
-      .split("&")
-      .reduce<Record<string, string>>((acc, param) => {
-        const [key, value] = param.split("=") as [string, string];
-
-        acc[key] = value;
-        return acc;
-      }, {});
-  },
-  z
-    .object({
-      access_token: z.string(),
-      token_type: z.literal("Bearer"),
-      expires_in: z.coerce.number(),
-      state: z.string(),
-    })
-    .or(
-      z.object({
-        error: z.string(),
-        state: z.string(),
-      }),
-    ),
-);
-
+// TODO: Refactor into a generic ssr-disabler component
+const NoSSR = dynamic(async () => import("@/ui/auth/callback/params.tsx"), {
+  ssr: false,
+});
 /**
  *
  */
 export default function Page() {
-  const params = responseParamsSchema.parse(window.location.hash);
-
-  const localState = localStorage.getItem("state");
-
-  if (localState !== params.state) {
-    throw new Error("Invalid state");
-  }
-
   return (
     <main>
-      <div>
-        {"error" in params ? (
-          <div>Failed to authenticate: {params.error}</div>
-        ) : (
-          Object.entries(params).map(([key, value]) => (
-            <div key={key}>
-              {key}: {value}
-            </div>
-          ))
-        )}
-      </div>
+      <NoSSR />
     </main>
   );
 }
---
+++ spotify-data-form/src/ui/auth/callback/params.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { z } from "zod";
+
+const responseParamsSchema = z.preprocess(
+  (val) => {
+    if (typeof val !== "string") {
+      throw new Error("Expected a string");
+    }
+
+    return val
+      .substring(1)
+      .split("&")
+      .reduce<Record<string, string>>((acc, param) => {
+        const [key, value] = param.split("=") as [string, string];
+
+        acc[key] = value;
+        return acc;
+      }, {});
+  },
+  z
+    .object({
+      access_token: z.string(),
+      token_type: z.literal("Bearer"),
+      expires_in: z.coerce.number(),
+      state: z.string(),
+    })
+    .or(
+      z.object({
+        error: z.string(),
+        state: z.string(),
+      }),
+    ),
+);
+
+export default function Params() {
+  const params = responseParamsSchema.parse(window.location.hash);
+
+  const localState = localStorage.getItem("state");
+
+  if (localState !== params.state) {
+    throw new Error("Invalid state");
+  }
+
+  return (
+    <div>
+      {"error" in params ? (
+        <div>Failed to authenticate: {params.error}</div>
+      ) : (
+        Object.entries(params).map(([key, value]) => (
+          <div key={key}>
+            {key}: {value}
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
